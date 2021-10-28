3253cf7cc1150c604e25c0612171c0a88778da1f
naufalfajar <naufalphew@gmail.com>
2021-10-28 15:36:22 +0700

    first upload

--- login_action.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-    
-    if($email=="anjay@gmail.com" && $password=="anjay")
-        echo("Login Berhasil");
-    else
-        echo("Login Gagal");
-?>
